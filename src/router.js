033d715d4013cb7a0ec63f6b9eddba6e65fbda11
Miguel Sanchez Gonzalez <miguelsanychez@gmail.com>
2018-08-08 06:57:05 +0100

    feat: Hash a string based on route parameters

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Hash from '@/views/Hash';
 
 Vue.use( Router );
 
@@ -12,6 +13,11 @@ export default new Router( {
             path: '/',
             name: 'home',
             component: Home
+        },
+        {
+            path: '/hash/:value',
+            name: 'hash',
+            component: Hash
         },
         {
             path: '/hash-value',
---
+++ src/views/Hash.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1 class="md-display-3">
+        {{ hash }}
+    </h1>
+</template>
+
+<script>
+
+import requestHash from '@/api/hash';
+
+export default {
+
+    name: 'hash',
+
+    data: () => ( {
+        hash: null
+    } ),
+
+    created () {
+        requestHash( this.$route.params.value )
+            .then( ( { hash } ) => this.hash = hash );
+    }
+
+};
+
+</script>
